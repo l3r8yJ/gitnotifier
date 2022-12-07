021b4170095a84537cd5910c6b7fc1bb76d66082
Ivan <clicker.heroes.acg@gmail.com>
2022-12-07 18:21:26 +0300

    start

---
+++ lib/git/notifier.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "notifier/version"
+
+module Git
+  module Notifier
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
