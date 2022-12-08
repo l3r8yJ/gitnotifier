6422d33d216fcb2bb239e7f51cf7ae55272727bf
Ivan <clicker.heroes.acg@gmail.com>
2022-12-08 13:36:33 +0300

    test bot impl

---
+++ lib/gitnotifier/user.rb
@@ -0,0 +1,49 @@
+# Copyright (c) 2022 Ivanchuck Ivan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'pg'
+require 'yaml'
+
+# The user class.
+# Author:: Ivanchuk Ivan (clicker.heroes.acg@gmail.com)
+# Copyright:: Copyright (c) 2022 Ivanchuck Ivan
+# License:: MIT
+class User
+  # @todo #7 Tests/ test user.
+  # We have to write integration and unit tests for User class.
+  def initialize(chat, github)
+    @chat = chat
+    @github = github
+    config = YAML.load_file('pg.yml')['database']
+    @pgcon = PGconn.new(
+      host: config['host'],
+      user: config['user'],
+      password: config['password'],
+      dbname: config['schema']
+    )
+  end
+
+  def save
+    @pgcon.exec(
+      'INSERT INTO bot_user(chat_id, token) VALUES ($1, $2)',
+      [@chat, @github]
+    )
+  end
+end
